7b72eec9e685ef8d1cbb80f3cc3761ee7016eba2
JesseOgoula <adirignoogoula@gmail.com>
2026-03-09 14:09:27 +0100

    feat: implement new transaction screen with custom input pads and receipt scanning capabilities.

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -13,7 +13,6 @@
 #include <local_auth_windows/local_auth_plugin.h>
 #include <powersync_flutter_libs/powersync_flutter_libs_plugin.h>
 #include <sentry_flutter/sentry_flutter_plugin.h>
-#include <speech_to_text_windows/speech_to_text_windows.h>
 #include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
@@ -32,8 +31,6 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("PowersyncFlutterLibsPlugin"));
   SentryFlutterPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
-  SpeechToTextWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("SpeechToTextWindows"));
   Sqlite3FlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
